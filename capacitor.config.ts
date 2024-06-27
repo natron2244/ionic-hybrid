594f460f458d3577ea923a24b1938247ba4ce4d5
Nathan Christensen <natron2244@gmail.com>
2024-06-27 10:58:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'module-based',
+  webDir: 'www'
+};
+
+export default config;
